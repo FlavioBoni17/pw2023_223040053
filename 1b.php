b72e88e725db3da5ae0e8ae28d3512811cce0fcf
FlavioBoni17 <flavioboni17@gmail.com>
2023-02-20 22:02:01 +0700

    Mengerjakan Tugas 1

---
+++ 1b.php
@@ -0,0 +1,25 @@
+<?php 
+$npm = 53;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1b</title>
+</head>
+<body>
+    <?php echo"Aku adalah angka <b> $npm </b>"; ?> <br>
+    <?php $npm = $npm * 5 ?>
+    <?php echo "Jika aku dikali 5, maka aku sekarang menjadi <b> $npm </b>"?> <br>
+    <?php $npm = $npm / 2 ?>
+    <?php echo"Jika aku dibagi 2, maka aku sekarang menjadi <b>$npm</b>"?> <br>
+    <?php $npm = $npm + 75 ?>
+    <?php echo"Jika aku ditambah 75, maka aku sekarang menjadi <b> $npm </b> "?> <br>
+    <?php $npm = $npm - 20 ?>
+    <?php echo " Jika aku dikurang 20, maka aku sekarang menjadi <b> $npm </b>" ?> 
+</body>
+</html>
+
